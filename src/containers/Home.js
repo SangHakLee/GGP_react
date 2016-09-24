03750a09dba6a11f741c1c77cf556a15d095abc5
SangHakLee <leesanghak7298@gmail.com>
2016-09-24 18:02:30 +0900

    키워드 입력창 완성

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,77 @@
 // src/containers/Home.js
 import React from 'react';
+import { connect } from 'react-redux';
+import { Write } from 'components';
+import { memoPostRequest } from 'actions/memo';
 
 class Home extends React.Component {
+  constructor(props) {
+      super(props);
+      this.handlePost = this.handlePost.bind(this);
+  }
+
+  /* POST MEMO */
+    handlePost(contents) {
+        return this.props.memoPostRequest(contents).then(
+            () => {
+                if(this.props.postStatus.status === "SUCCESS") {
+                    // TRIGGER LOAD NEW MEMO
+                    // TO BE IMPLEMENTED
+                    Materialize.toast('Success!', 2000);
+                } else {
+                    /*
+                        ERROR CODES
+                            1: NOT LOGGED IN
+                            2: EMPTY CONTENTS
+                    */
+                    let $toastContent;
+                    switch(this.props.postStatus.error) {
+                        case 1:
+                            // IF NOT LOGGED IN, NOTIFY AND REFRESH AFTER
+                            $toastContent = $('<span style="color: #FFB4BA">로그인 먼저 해주세요</span>');
+                            Materialize.toast($toastContent, 2000);
+                            setTimeout(()=> {location.reload(false);}, 2000);
+                            break;
+                        case 2:
+                            $toastContent = $('<span style="color: #FFB4BA">내용을 써주세요.</span>');
+                            Materialize.toast($toastContent, 2000);
+                            break;
+                        default:
+                            $toastContent = $('<span style="color: #FFB4BA">에러</span>');
+                            Materialize.toast($toastContent, 2000);
+                            break;
+                    }
+                }
+            }
+        );
+    }
+
+
+
     render() {
+        const write = ( <Write onPost={this.handlePost}/> );
         return (
-            <div>
-                Home
+            <div className="wrapper">
+                { this.props.isLoggedIn ? write : undefined }
             </div>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+        isLoggedIn: state.authentication.status.isLoggedIn,
+        postStatus: state.memo.post
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        memoPostRequest: (contents) => {
+            return dispatch(memoPostRequest(contents));
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
